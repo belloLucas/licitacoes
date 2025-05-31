538f0f5ed0288e6ed3251f5caa1950c331c1950e
belloLucas <lucasbellodev@gmail.com>
2025-05-31 09:37:19 -0300

    starting project

---
+++ backend/src/test/java/com/bellolucas/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellolucas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
