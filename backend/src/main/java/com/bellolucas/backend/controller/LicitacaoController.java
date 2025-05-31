e8459aef628b6590b6d628081371a952b3159f33
belloLucas <lucasbellodev@gmail.com>
2025-05-31 13:03:23 -0300

    feat: creating licitacacao controller

---
+++ backend/src/main/java/com/bellolucas/backend/controller/LicitacaoController.java
@@ -0,0 +1,32 @@
+package com.bellolucas.backend.controller;
+
+import com.bellolucas.backend.model.Licitacao;
+import com.bellolucas.backend.service.LicitacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/licitacoes")
+public class LicitacaoController {
+    @Autowired
+    private LicitacaoService service;
+
+    @GetMapping
+    public List<Licitacao> list() {
+        return service.list();
+    }
+
+    @GetMapping("/uasg/{uasgCodigo}")
+    public Licitacao listByUasgCodigo(String uasgCodigo) {
+        return service.listByUasgCodigo(uasgCodigo);
+    }
+
+    @GetMapping("/pregao/{numeroPregao}")
+    public Licitacao listByNumeroPregao(String numeroPregao) {
+        return service.listByNumeroPregao(numeroPregao);
+    }
+}
