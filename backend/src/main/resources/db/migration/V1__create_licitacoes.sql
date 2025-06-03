681f908074344fa87e84511dd2f2f040274a22c6
belloLucas <lucasbellodev@gmail.com>
2025-06-03 14:42:28 -0300

    fix: removing comma on last line to avoid error

--- backend/src/main/resources/db/migration/V1__create_licitacoes.sql
+++ backend/src/main/resources/db/migration/V1__create_licitacoes.sql
@@ -1,5 +1,5 @@
 CREATE TABLE licitacoes (
-    id SERIAL PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     uasg_codigo VARCHAR(10) NOT NULL,
     numero_pregao VARCHAR(20) NOT NULL,
     descricao TEXT NOT NULL,
@@ -7,5 +7,6 @@ CREATE TABLE licitacoes (
     endereco TEXT NOT NULL,
     telefone VARCHAR(30),
     fax VARCHAR(30),
-    data_abertura DATE
+    data_abertura DATE,
+    itens_url VARCHAR(255)
 );
