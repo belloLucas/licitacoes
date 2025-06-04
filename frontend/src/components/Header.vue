e2279177cff75b50b8468313d62f94b2dc2d86db
belloLucas <lucasbellodev@gmail.com>
2025-06-03 22:13:46 -0300

    feat: creating header component

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <header class="bg-green-700 text-white p-4 shadow-md">
+    <div class="container mx-auto">
+      <h1 class="text-2xl font-bold flex items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          class="w-6 h-6"
+        >
+          <path d="M3 3v18h18" />
+          <path d="m19 9-5 5-4-4-3 3" />
+        </svg>
+        Licitações do Governo Federal
+      </h1>
+    </div>
+  </header>
+</template>
