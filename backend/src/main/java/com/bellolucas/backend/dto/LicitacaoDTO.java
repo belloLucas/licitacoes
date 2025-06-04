531334ff201937640542103e2b49b33d0a1e3efb
belloLucas <lucasbellodev@gmail.com>
2025-06-03 22:12:09 -0300

    feat: passing new information on DTO

--- backend/src/main/java/com/bellolucas/backend/dto/LicitacaoDTO.java
+++ backend/src/main/java/com/bellolucas/backend/dto/LicitacaoDTO.java
@@ -5,6 +5,7 @@ import com.bellolucas.backend.model.Licitacao;
 import java.time.format.DateTimeFormatter;
 
 public record LicitacaoDTO(
+        Long id,
         String codigoUasg,
         String numeroPregao,
         String descricao,
@@ -12,10 +13,12 @@ public record LicitacaoDTO(
         String endereco,
         String telefone,
         String fax,
-        String dataEntregaProposta
+        String dataEntregaProposta,
+        String itemsUrl
 ) {
     public LicitacaoDTO(Licitacao licitacao) {
         this(
+                licitacao.getId(),
                 licitacao.getUasgCodigo(),
                 licitacao.getNumeroPregao(),
                 licitacao.getDescricao(),
@@ -23,7 +26,8 @@ public record LicitacaoDTO(
                 licitacao.getEndereco(),
                 licitacao.getTelefone(),
                 licitacao.getFax(),
-                licitacao.getDataAbertura() != null ? licitacao.getDataAbertura().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) : null
+                licitacao.getDataAbertura() != null ? licitacao.getDataAbertura().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) : null,
+                licitacao.getItensUrl()
         );
     }
 }
